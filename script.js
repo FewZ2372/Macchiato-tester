f7d2de0881c41f9ad398b6faa61cb3d22c74c67a
FewZ2372 <Felipebxeneize@gmail.com>
2023-12-14 17:23:51 -0300

    Again

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 window.addEventListener('load', function () {
-    var scene = document.querySelector('a-scene');
-    if (scene) {
-        scene.addEventListener('loaded', function () {
-            var loadingScreen = document.getElementById('loadingScreen');
-            loadingScreen.classList.add('fade-out');
-        });
-    }
+    setTimeout(function () {
+        var loadingScreen = document.getElementById('loadingScreen');
+        loadingScreen.classList.add('fade-out');
+    }, 3000); // Change 3000 to the number of milliseconds you want the loading screen to appear
 });
